8c1880df1decf9d493f06b0ccdb9297709cdab17
saadq12 <saadqazi23@gmail.com>
2016-10-17 19:33:59 -0400

    SDK,Background,Font
    changed targetSDK version to 23, added a backgroudn image to the
    acitivitys, added custom assets>fonts folder for custom fonts.

--- IndusTechApp/app/src/main/java/com/humber/industech/industechapp/MainActivity.java
+++ IndusTechApp/app/src/main/java/com/humber/industech/industechapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.humber.industech.industechapp;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,11 +14,18 @@ public class MainActivity extends AppCompatActivity {
     private Button ScanButton;
     private Button DataButton;
     private Button RTVButton;
+    private TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //setting custom font
+        t = (TextView) findViewById(R.id.textView);
+        Typeface customFont = Typeface.createFromAsset(getAssets(),"fonts/Prezident.ttf");
+        t.setTypeface(customFont);
 
         //setting click listener to the scan button so it can go to a ScanActivity
         ScanButton = (Button) findViewById(R.id.ScanBtn);
@@ -48,6 +57,8 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(RtvButtonIntent);
             }
         });
+
+
 
 
 
---
+++ IndusTechApp/spinnerexample/src/main/java/qazi/saad/spinnerexample/CustomOnItemSelectedListener.java
@@ -0,0 +1,28 @@
+package qazi.saad.spinnerexample;
+
+/**
+ * Created by Saad on 2016-10-04.
+ */
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.Toast;
+
+public class CustomOnItemSelectedListener implements OnItemSelectedListener {
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos,
+                               long id) {
+
+        Toast.makeText(parent.getContext(),
+                "On Item Select : \n" + parent.getItemAtPosition(pos).toString(),
+                Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
+
