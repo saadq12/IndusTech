e5ab1fce8526a949c73b39487e0afc239290a28c
Abhirup Das <abhirupdas033@gmail.com>
2016-10-18 18:25:35 -0400

    Update Machine_list.java

--- industrial_revolution_4.0/src/main/java/inc/race/industrial_revolution_40/Machine_list.java
+++ industrial_revolution_4.0/src/main/java/inc/race/industrial_revolution_40/Machine_list.java
@@ -1,3 +1,9 @@
+/**
+ * Project name: Industrial Revolution 4.0
+ * Team name: IndusTech
+ * Members: Abhirup Das, Saad Qazi and Ratha Ariyanayagam
+ */
+
 package inc.race.industrial_revolution_40;
 
 import android.support.v7.app.AppCompatActivity;
@@ -10,4 +16,12 @@ public class Machine_list extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_machine_list);
     }
+    
+    public	void	back(View view)	{
+        Intent intent	=	null;
+        intent	=	new	Intent(Machine_list.this, Main.class);
+        startActivity(intent);
+
+
+    }		//	end	of	AndroidTestAcDvity
 }
