d26eb1177ecd452566730da5a766bc23574a2ba3
nightmarebat <ratha.ariaya_00@hotmail.ca>
2016-10-18 14:26:18 -0400

    ratha - added
    change icon

--- industrial_revolution_4.0/src/main/java/inc/race/industrial_revolution_40/Scan.java
+++ industrial_revolution_4.0/src/main/java/inc/race/industrial_revolution_40/Scan.java
@@ -2,15 +2,11 @@ package inc.race.industrial_revolution_40;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
-
-import com.google.zxing.Result;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
@@ -42,12 +38,12 @@ public class Scan extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
-        if (result != null) {
-            if (result.getContents() == null) {
+        IntentResult output = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (output != null) {
+            if (output.getContents() == null) {
                 Toast.makeText(this, " You cancelled the Scan", Toast.LENGTH_LONG).show();
             } else {
-                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                Toast.makeText(this, output.getContents(), Toast.LENGTH_LONG).show();
             }
 
         }
