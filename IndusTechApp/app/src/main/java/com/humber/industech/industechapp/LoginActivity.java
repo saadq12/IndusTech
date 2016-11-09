d508bb4d435a0473f07c679a0c6baa08580c09e6
saadq12 <saadqazi23@gmail.com>
2016-11-08 20:20:13 -0500

    Updated to git hub REPO and LoginActivity.java
    updated AndroidManifest.xml file to change first activity to
    LoginActivity.java
    "click to login" button will naviatge to MainActivity.
    still NO backend code impleneted to log user in.
    got rid of uneeded modules in the IndusTechApp folder located in the git
    hub repo.

--- IndusTechApp/app/src/main/java/com/humber/industech/industechapp/LoginActivity.java
+++ IndusTechApp/app/src/main/java/com/humber/industech/industechapp/LoginActivity.java
@@ -1,21 +1,41 @@
 package com.humber.industech.industechapp;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     private TextView t;
+    private Button loginbttn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
         //setting custom font
-        t = (TextView) findViewById(R.id.textView);
+        t = (TextView) findViewById(R.id.textView6);
         Typeface customFont = Typeface.createFromAsset(getAssets(),"fonts/Prezident.ttf");
         t.setTypeface(customFont);
+
+
+        //assign button to layout
+        loginbttn = (Button)findViewById(R.id.loginbutton);
+        loginbttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(LoginActivity.this, "Login button clicked", Toast.LENGTH_SHORT).show();
+                Intent loginintent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(loginintent);
+            }
+        });
+
     }
 }
